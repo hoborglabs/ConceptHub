2b26443094ee9b4595f360a747d3bc27cec9b248
Wojtek Oledzki <wojciech.oledzki@bskyb.com>
2012-03-08 21:33:34 +0000

    hot fix

--- public/folder.php
+++ public/folder.php
@@ -3,7 +3,7 @@ $folder = $_GET['f'];
 $home = realpath(__DIR__);
 $target = realpath($home . '/' . $folder);
 
-if (false === strpos($target, $home) || $home = $target) {
+if (false === strpos($target, $home) || $home == $target) {
 	echo json_encode(array(array('src' => '?', 'name' => 'are you kidding me?')));
 	return;
 }
